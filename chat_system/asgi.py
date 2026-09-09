6d58dad5d5283c3a764f5a011d759406baeb51c7
Maryam Al Menshawy <maryammanshawy@gmail.com>
2026-09-10 01:53:08 +0300

    Fix ASGI settings

--- chat_system/asgi.py
+++ chat_system/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_system.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
